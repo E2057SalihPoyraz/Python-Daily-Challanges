8f4d53ebf789cf8441bbcffe88e1084397d6e7e4
Salih Poyraz <ericc.star023@gmail.com>
2020-10-27 00:15:25 +0300

    power-set.py loaded.

---
+++ power-set.py
@@ -0,0 +1,14 @@
+# QUESTION:
+# This is an interview question asked by Google.
+# The power set of a set is the set of all its subsets. Write a function that, given a set,
+# generates its power set.
+# For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3},
+# {2, 3}, {1, 2, 3}}.
+# You may also use a list or array to represent a set.
+
+def f_subsets(s):
+    result = []
+    for i in s:
+        result += [j.union({i}) for j in result]
+        result.append({i})
+    return [{}] + sorted(result, key = len)
