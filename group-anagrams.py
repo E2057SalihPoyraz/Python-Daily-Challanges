3d6d928603148469f89d302ab707ea173cd6787e
Salih Poyraz <ericc.star023@gmail.com>
2020-12-26 01:01:31 +0300

    modified group-anagrams.py

--- group-anagrams.py
+++ group-anagrams.py
@@ -64,3 +64,21 @@ for i in A:
     C=[]
 print(B)
 
+#5 :
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+anag = {}
+for i in strs:
+    if "".join(sorted(i)) in anag:
+        anag["".join(sorted(i))].append(i)
+    else:
+        anag["".join(sorted(i))] = [i]
+print(list(anag.values()))
+
+#6 :
+strings = ["eat", "tea", "tan", "ate", "nat", "bat"]
+result = []
+for j in strings:
+    item = [x for x in strings if sorted(j) == sorted(x)]
+    if item not in result:
+        result.append(item)
+print(result)
