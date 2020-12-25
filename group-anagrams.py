bb26bc5376a094551ebf484ba0be91b1a2c301d9
Salih Poyraz <ericc.star023@gmail.com>
2020-12-25 21:42:33 +0300

    group-anagrams.py

---
+++ group-anagrams.py
@@ -0,0 +1,66 @@
+Group Anagrams
+Given a list of strings, group anagrams together.
+Example:
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"]
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+All inputs will be in lowercase.
+The order of your output does not matter.
+============================================
+
+#1:
+def anagram(Input):
+    a = {}
+    for i in Input:
+        x = "".join(sorted(i))
+        if x in a:
+            a[x].append(i)
+        else:
+            a[x] = [i]
+    return list(a.values())
+print(anagram(Input))
+
+#2:
+def anagram(lst):
+    a,b,c=[], [], []
+    for i in lst:
+        if sorted(i) not in a:
+            a.append(sorted(list(i)))
+            b.append([])
+    for j in range(len(b)):
+        for k in lst:
+            if sorted(k)==a[j]: b[j].append(k)
+    for m in sorted(b, key=len, reverse=True):
+        c.append(sorted(m))
+    return c
+anagram(["eat", "tea", "tan", "ate", "nat", "bat"])
+
+#3:
+def groupAnagrams(strs):
+    anagrams = {}
+    for i in strs:
+        item = "".join(sorted(i))
+        if item in anagrams:
+            anagrams[item].append(i)
+        else:
+            anagrams[item]=[i]
+    return anagrams.values()
+
+#4:
+A=["eat", "tea", "tan", "ate", "nat", "bat"]
+C=[]
+B=[]
+for i in A:
+    for j in A:
+        if set(i)==set(j):
+            C.append(j)
+    if not C in B:
+        B.append(C)
+    C=[]
+print(B)
+
