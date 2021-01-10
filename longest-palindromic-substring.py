24c3e2195d7879ca51210bea1e149814c28e9557
Salih Poyraz <ericc.star023@gmail.com>
2021-01-10 21:18:28 +0300

    modified:   longest-palindromic-substring.py

--- longest-palindromic-substring.py
+++ longest-palindromic-substring.py
@@ -16,3 +16,15 @@ def long_palin(txt):
                 if not t in a:a.append(t)
     return(sorted(a, key=len, reverse = True)[0])        
 long_palin("million") 
+
+#2 :
+x = "million"
+a = ""
+b = {}
+for i in x:
+    a += i
+    if a in x[::-1]:
+        b[a] = len(a)
+    elif a != a[::-1]:
+        a = a[1:]
+max(b.keys(), key= lambda k: b[k])
